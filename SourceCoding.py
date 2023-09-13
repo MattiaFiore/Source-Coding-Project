68ef53af4207e5fb8089c02816200a38348a4f00
MattiaFiore <67999260+MattiaFiore@users.noreply.github.com>
2023-09-13 23:00:31 +0200

    Update SourceCoding.py

--- SourceCoding.py
+++ SourceCoding.py
@@ -179,6 +179,23 @@ class Interval():
 
     self.feasible_range = self.feasible_range[indice_min:indice_max+1]
 
+  def trim_ranges_opt(self, bit):
+
+    if bit == '0':
+
+      for i in range(len(self.feasible_range), 0, -1):
+      if self.range[1] <= self.feasible_range[i-1]:
+        # se è più piccolo dell'estremo sinistro dell'intervalo
+        indice_max -= 1
+        
+    else: 
+
+      for i in range(len(self.feasible_range)-1):
+      if self.range[0] >= self.feasible_range[i+1]:
+        # se l'intervallo è più piccolo dell'estremo destro dell'intervallo
+        indice_min += 1
+      
+
   def reset(self):
 
     self.symbol_index += 1
@@ -201,7 +218,7 @@ class Interval():
 
       # verify which intervals are feasible
       # now we need to trim the
-      self.trim_ranges()
+      self.trim_ranges_opt(bit)
 
   def decode(self):
 
